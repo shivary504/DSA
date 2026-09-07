86429a461ad8f22039be8d4c096c17b3c771d44e
Shivansh Maheshwari <shivanshmaheshwari001@gmail.com>
2026-09-07 23:55:19 +0530

    Transform binary tree to library catalog management
    Refactor binary tree to a library catalog system with book insertion, deletion, and display functionality.

--- 5a.py
+++ 5a.py
@@ -1,63 +1,108 @@
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-
-def insert(root, data):
-    if root is None:
-        return Node(data)
-
-    if data < root.data:
-        root.left = insert(root.left, data)
-    elif data > root.data:
-        root.right = insert(root.right, data)
-
-    return root
-
-def inorder(root):
-    stack = []
-    current = root
-
-    while current is not None or stack:
-        
-        while current is not None:
-            stack.append(current)
-            current = current.left
-
-        current = stack.pop()
-        print(current.data, end=" ")
-
-        current = current.right
-
-def preorder(root):
-    if root is None:
-        return
-
-    stack = [root]
-
-    while stack:
-        current = stack.pop()
-
-        print(current.data, end=" ")
-
-        if current.right is not None:
-            stack.append(current.right)
-
-        if current.left is not None:
-            stack.append(current.left)
-
-root = None
-
-n = int(input("Enter number of nodes: "))
-
-for i in range(n):
-    data = int(input("Enter node value: "))
-    root = insert(root, data)
+    def __init__(self, book):
+        self.book = book
+        self.next = None
 
 
-print("\nInorder Traversal:")
-inorder(root)
+class Library:
+    def __init__(self):
+        self.head = None
 
-print("\nPreorder Traversal:")
-preorder(root)
+    def insert(self, book, position):
+        new_node = Node(book)
+
+        if position == 1:
+            new_node.next = self.head
+            self.head = new_node
+            return
+
+        temp = self.head
+
+        for i in range(1, position - 1):
+            if temp is None:
+                print("Invalid position!")
+                return
+            temp = temp.next
+
+        if temp is None:
+            print("Invalid position!")
+            return
+
+        new_node.next = temp.next
+        temp.next = new_node
+
+    # Delete from any position
+    def delete(self, position):
+
+        if self.head is None:
+            print("Library catalog is empty!")
+            return
+
+        if position == 1:
+            print("Deleted:", self.head.book)
+            self.head = self.head.next
+            return
+
+        temp = self.head
+
+        for i in range(1, position - 1):
+            if temp is None:
+                print("Invalid position!")
+                return
+            temp = temp.next
+
+        if temp is None or temp.next is None:
+            print("Invalid position!")
+            return
+
+        print("Deleted:", temp.next.book)
+
+        temp.next = temp.next.next
+
+    def display(self):
+        if self.head is None:
+            print("Library catalog is empty!")
+            return
+
+        temp = self.head
+
+        print("Library Catalog:")
+
+        while temp:
+            print(temp.book, end=" -> ")
+            temp = temp.next
+
+        print("None")
+
+
+library = Library()
+
+while True:
+    print("\n--- Library Catalog ---")
+    print("1. Insert Book")
+    print("2. Delete Book")
+    print("3. Display")
+    print("4. Exit")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        book = input("Enter book name: ")
+        position = int(input("Enter position: "))
+
+        library.insert(book, position)
+
+    elif choice == 2:
+        position = int(input("Enter position to delete: "))
+
+        library.delete(position)
+
+    elif choice == 3:
+        library.display()
+
+    elif choice == 4:
+        print("Exiting...")
+        break
+
+    else:
+        print("Invalid choice!")
